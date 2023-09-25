81439a9de426ce5eae67154e980e8a7b54358a6d
codingobliqua <andres.asencios10@gmail.com>
2023-09-25 15:55:54 -0700

    addede db name

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 
 // Define your database connection here
-const sequelize = new Sequelize('your_database_name', 'root', 'Changeme', {
+const sequelize = new Sequelize('techblogdb', 'root', 'Changeme', {
   host: 'localhost', 
   dialect: 'mysql',
 });
